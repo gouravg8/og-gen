ad6ec7eed9d0872b8371f654551e190e4298aff5
Gourav soni <gouravsoni8476@gmail.com>
2024-08-11 22:30:29 +0530

    added server actions

---
+++ frontend/app/actions/post.ts
@@ -0,0 +1,35 @@
+"use server"
+import client from '@/db'
+import fs from 'fs/promises'
+
+export async function getAllPosts() {
+    try {
+        const posts = await client.post.findMany()
+        return { message: posts, error: false };
+    } catch (error) {
+        return { message: 'Can not get posts', error }
+    }
+}
+
+export async function getPostById(id: number) {
+    try {
+        const post = await client.post.findFirst({ where: { id } })
+        return { message: post, error: false };
+    } catch (error) {
+        return { message: 'Can not get post by id', error }
+    }
+}
+
+export async function initDb() {
+    try {
+        const data = JSON.parse(await fs.readFile('./posts.json', 'utf-8'))
+        console.log(data);
+
+        const posts = await client.post.createMany({
+            data
+        })
+        return { message: true, posts }
+    } catch (error) {
+        return { message: false, error }
+    }
+}
