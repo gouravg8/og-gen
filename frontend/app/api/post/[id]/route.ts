f84a6a84a9f9ac902fdac9062b6faa021a9c23f1
Gourav soni <gouravsoni8476@gmail.com>
2024-08-14 19:41:17 +0530

    fix types: from number to string

--- frontend/app/api/post/[id]/route.ts
+++ frontend/app/api/post/[id]/route.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getPostById } from "@/app/actions/post";
 
-export async function GET(req: NextRequest, { params }: { params: { id: number } }) {
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
     const { id } = params;
-    console.log('id', id, 'type: ', typeof id);
-    console.log('req', req.nextUrl, params);
 
     if (id) {
-        const post = await getPostById(Number(id));
+        const post = await getPostById(id);
         return NextResponse.json({ msg: post.message, error: post.error }, { status: 200 })
     }
 }
